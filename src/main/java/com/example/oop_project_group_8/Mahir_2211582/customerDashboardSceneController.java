fbbd7f9489f4a1b0a734586109f278c402ce9ad7
mash106 <mdmashur001@gmail.com>
2024-12-12 13:13:58 +0600

    Mahir dashboard fxml and created controller class

---
+++ src/main/java/com/example/oop_project_group_8/Mahir_2211582/customerDashboardSceneController.java
@@ -0,0 +1,42 @@
+package com.example.oop_project_group_8.Mahir_2211582;
+
+import javafx.event.ActionEvent;
+
+public class customerDashboardSceneController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void contactSupportButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void orderHistoryButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void placeOrderButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitReviewButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void browseProductsButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void requestRefundButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackOrderButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewProductsButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/oop_project_group_8/Mahir_2211582/inventoryManagerDashboardSceneController.java
@@ -0,0 +1,42 @@
+package com.example.oop_project_group_8.Mahir_2211582;
+
+import javafx.event.ActionEvent;
+
+public class inventoryManagerDashboardSceneController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void lowStockButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createReportButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorInventoryButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageSupplierButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateItemsButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackCostButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addItemsButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reorderButtonOnClickedDB(ActionEvent actionEvent) {
+    }
+}
